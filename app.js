eb53c3ae81a92ee769a220a2524752fafce3a667
Elisabeth Gross <elisabethgross568@gmail.com>
2016-06-20 12:56:18 -0400

    working on tags

---
+++ app.js
@@ -0,0 +1,67 @@
+'use strict';
+var express = require('express');
+var app = express();
+var morgan = require('morgan');
+var swig = require('swig');
+var makesRouter = require('./routes');
+var fs = require('fs');
+var path = require('path');
+var mime = require('mime');
+var bodyParser = require('body-parser');
+var socketio = require('socket.io');
+
+// templating boilerplate setup
+app.engine('html', swig.renderFile); // how to render html templates
+app.set('view engine', 'html'); // what file extension do our templates have
+app.set('views', path.join(__dirname, '/views')); // where to find the views
+swig.setDefaults({ cache: false });
+
+// logging middleware
+app.use(morgan('dev'));
+
+// body parsing middleware
+app.use(bodyParser.urlencoded({ extended: true })); // for HTML form submits
+app.use(bodyParser.json()); // would be for AJAX requests
+
+
+// start the server
+var server = app.listen(1337, function(){
+  console.log('listening on port 1337');
+});
+var io = socketio.listen(server);
+
+app.use(express.static(path.join(__dirname, '/public')));
+
+// modular routing that uses io inside it
+app.use('/', makesRouter(io));
+
+// // manually-written static file middleware
+// app.use(function(req, res, next){
+//   var mimeType = mime.lookup(req.path);
+//   fs.readFile('./public' + req.path, function(err, fileBuffer){
+//     if (err) return next();
+//     res.header('Content-Type', mimeType);
+//     res.send(fileBuffer);
+//   });
+// });
+var pg = require('pg');
+var conString = "postgres://username:password@localhost/database";
+
+//this initializes a connection pool
+//it will keep idle connections open for a (configurable) 30 seconds
+//and set a limit of 10 (also configurable)
+pg.connect(conString, function(err, client, done) {
+  if(err) {
+    return console.error('error fetching client from pool', err);
+  }
+  client.query('SELECT $1::int AS number', ['1'], function(err, result) {
+    //call `done()` to release the client back to the pool
+    done();
+
+    if(err) {
+      return console.error('error running query', err);
+    }
+    console.log(result.rows[0].number);
+    //output: 1
+  });
+});
---
+++ routes/index.js
@@ -0,0 +1,114 @@
+'use strict';
+var express = require('express');
+var router = express.Router();
+var tweetBank = require('../tweetBank');
+var client = require('../db');
+
+
+module.exports = function makeRouterWithSockets (io) {
+
+  // a reusable function
+  function respondWithAllTweets (req, res, next){
+    client.query('SELECT * FROM tweets INNER JOIN users ON tweets.userid = users.id', function (err, result) {
+      if (err) return next(err);
+      var tweets = result.rows;
+      res.render('index', { title: 'Twitter.js', tweets: tweets, showForm: true});
+    });
+    // res.render('index', {
+    //   title: 'Twitter.js',
+    //   tweets: allTheTweets,
+    //   showForm: true
+    // });
+  }
+
+  // here we basically treet the root view and tweets view as identical
+  router.get('/', respondWithAllTweets);
+  router.get('/tweets', respondWithAllTweets);
+
+  // single-user page
+  router.get('/users/:username', function(req, res, next){
+    client.query('SELECT * FROM tweets INNER JOIN users ON tweets.userid = users.id WHERE users.name =$1', [req.params.username], function (err, result) {
+      if (err) return next(err);
+      var tweetsForName = result.rows;
+      res.render('index', {
+        title: 'Twitter.js',
+        tweets: tweetsForName,
+        showForm: true,
+        username: req.params.username
+      });
+    });
+  });
+
+  // single-tweet page
+  router.get('/tweets/:id', function(req, res, next){
+    client.query('SELECT * FROM tweets INNER JOIN users ON tweets.userid = users.id WHERE tweets.id =$1', [req.params.id], function (err, result) {
+      if (err) return next(err);
+      var tweetsWithThatId = result.rows;
+      res.render('index', {
+        title: 'Twitter.js',
+        tweets: tweetsWithThatId // an array of only one element ;-)
+      });
+    });
+  });
+
+  // create a new tweet
+  router.post('/tweets', function(req, res, next){
+      var username = req.body.name;
+      var userId;
+      // check if user is in the database
+      client.query('SELECT * FROM users ORDER BY id DESC', function(err, result) {
+        if (err) return next(err);
+        var userTable = result.rows;
+        for (var i = 0; i < userTable; i++) {
+          console.log('checking name');
+          if (username === userTable[i].name) {
+            userId = userTable[i].id;
+          }
+        }
+        if (!userId) {
+          console.log(userTable);
+          userId = userTable[0].id + 1;
+          client.query('INSERT INTO users (id, name) VALUES ($1, $2)', [userId, username]);
+        }
+        // check content for tags
+        var tweetContent = req.body.content;
+        var tag = tweetContent.substring(tweetContent.indexOf('#') + 1);
+        client.query('INSERT INTO tweets (content, userid) VALUES ($1, $2)', [req.body.content, userId], function () {
+          // add tags
+          // check if tag exists
+          client.query('SELECT * FROM tweets ORDER BY id DESC', function(err3, alltweets) {
+            if (err3) return next(err3);
+            var tweetsTable = alltweets.rows;
+            var tweetId = tweetsTable[0].id;
+              client.query('SELECT * FROM tags ORDER BY id DESC', function(err2, alltags) {
+                if (err2) return next(err2);
+                var tagId;
+                  var tagsTable = alltags.rows;
+                  for (var j = 0; j < tagsTable; j++) {
+                    console.log('checking tag');
+                    if (tag === tagsTable[j].name) {
+                      tagId = tagsTable[j].id;
+                    }
+                  }
+                  if (!tagId) {
+                    console.log(tagsTable);
+                    client.query('INSERT INTO tags (name, tweetId) VALUES ($1, $2)', [tag, tweetId]);
+                  }
+              });
+            });
+              console.log(userId);
+              console.log('insert');
+              // var newTweet = tweetBank.add(req.body.name, req.body.content);
+              // io.sockets.emit('new_tweet', newTweet);
+              res.redirect('/');
+           });
+         });
+      })
+
+  // // replaced this hard-coded route with general static routing in app.js
+  // router.get('/stylesheets/style.css', function(req, res, next){
+  //   res.sendFile('/stylesheets/style.css', { root: __dirname + '/../public/' });
+  // });
+
+  return router;
+}
